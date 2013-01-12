822d2ca54907ce06f2cb126d6e1e78193d899f8f
zunda <zunda at freeshell.org>
2013-01-11 16:56:35 -1000

    Created DropMemoWidgetProvider

--- src/com/edamametech/android/DropMemo/DropMemoEditWidgetProvider.java
+++ src/com/edamametech/android/DropMemo/DropMemoEditWidgetProvider.java
@@ -1,32 +1,15 @@
 
 package com.edamametech.android.DropMemo;
 
-import java.util.Date;
-
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
-import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.RemoteViews;
 
-public class DropMemoEditWidgetProvider extends AppWidgetProvider {
+public class DropMemoEditWidgetProvider extends DropMemoWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        final int NWidgets = appWidgetIds.length;
-        DropMemoUtil.FilePath path = new DropMemoUtil.FilePath(new Date());
-        Intent intent = new Intent(Intent.ACTION_EDIT);
-        intent.setDataAndType(path.uri(), "text/plain");
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.edit);
-        for (int i = 0; i < NWidgets; i++) {
-            int appWidgetId = appWidgetIds[i];
-            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
-            views.setTextViewText(R.id.widget_label, path.directoryName() + "/\n"
-                    + path.filename());
-            views.setOnClickPendingIntent(R.id.widget_label, pendingIntent);
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        intent = new Intent(Intent.ACTION_EDIT);
+        layout = R.layout.edit;
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 }
---
+++ src/com/edamametech/android/DropMemo/DropMemoWidgetProvider.java
@@ -0,0 +1,34 @@
+
+package com.edamametech.android.DropMemo;
+
+import java.util.Date;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class DropMemoWidgetProvider extends AppWidgetProvider {
+    Intent intent;
+    Integer layout;
+    
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int NWidgets = appWidgetIds.length;
+        DropMemoUtil.FilePath path = new DropMemoUtil.FilePath(new Date());
+        intent.setDataAndType(path.uri(), "text/plain");
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        RemoteViews views = new RemoteViews(context.getPackageName(), layout);
+        for (int i = 0; i < NWidgets; i++) {
+            int appWidgetId = appWidgetIds[i];
+            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+            views.setTextViewText(R.id.widget_label, path.directoryName() + "/\n"
+                    + path.filename());
+            views.setOnClickPendingIntent(R.id.widget_label, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
