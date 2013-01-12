9e84181cbfb72bbf12d8f4f771c518241acff33c
zunda <zunda at freeshell.org>
2013-01-11 16:41:54 -1000

    Refactored

--- src/com/edamametech/android/DropMemo/DropMemoAppWidgetProvider.java
+++ src/com/edamametech/android/DropMemo/DropMemoAppWidgetProvider.java
@@ -2,44 +2,31 @@
 package com.edamametech.android.DropMemo;
 
 import java.util.Date;
-import java.text.SimpleDateFormat;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
 import android.widget.RemoteViews;
 
 public class DropMemoAppWidgetProvider extends AppWidgetProvider {
-    final String filenameFormat = "yyMMdd'.txt'";
-    SimpleDateFormat filenameFormatter = new SimpleDateFormat(filenameFormat);
-    String directory = Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOWNLOADS).toString();
-    String[] directoryNameAry = directory.split("/");
-    String directoryName = directoryNameAry[directoryNameAry.length - 1];
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         final int NWidgets = appWidgetIds.length;
-        String currentFilename = filename(new Date());
+        DropMemoUtil.FilePath path = new DropMemoUtil.FilePath(new Date());
         Intent intent = new Intent(Intent.ACTION_EDIT);
-        intent.setDataAndType(Uri.parse("file:/" + directory + "/" + currentFilename), "text/plain");
+        intent.setDataAndType(path.uri(), "text/plain");
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
         for (int i = 0; i < NWidgets; i++) {
             int appWidgetId = appWidgetIds[i];
             views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
-            views.setTextViewText(R.id.widget_label, directoryName + "/\n" + currentFilename);
+            views.setTextViewText(R.id.widget_label, path.directoryName() + "/\n"
+                    + path.filename());
             views.setOnClickPendingIntent(R.id.widget_label, pendingIntent);
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-    }
-
-    String filename(Date date) {
-        return filenameFormatter.format(date);
     }
 }
---
+++ src/com/edamametech/android/DropMemo/DropMemoUtil.java
@@ -0,0 +1,39 @@
+
+package com.edamametech.android.DropMemo;
+
+import android.net.Uri;
+import android.os.Environment;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DropMemoUtil {
+    static public class FilePath {
+        private final String filenameFormat = "yyMMdd'.txt'";
+        private final SimpleDateFormat filenameFormatter = new SimpleDateFormat(filenameFormat);
+        Date date;
+
+        FilePath(Date d) {
+            date = d;
+        }
+
+        String directory() {
+            return Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS).toString();
+        }
+
+        String filename() {
+            return filenameFormatter.format(date);
+        }
+
+        String directoryName() {
+            String[] directoryNameAry = directory().split("/");
+            return directoryNameAry[directoryNameAry.length - 1];
+        }
+
+        Uri uri() {
+            return Uri.parse("file:/" + directory() + "/" + filename());
+        }
+    }
+
+}
